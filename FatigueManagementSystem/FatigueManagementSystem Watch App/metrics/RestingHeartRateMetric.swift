fd84fe1b6fa5546abd3cfa47e8092ad467426eb3
s3989834 <s3989834@student.rmit.edu.au>
2025-09-16 15:45:10 +1000

    writing restingheartrate

---
+++ FatigueManagementSystem/FatigueManagementSystem Watch App/metrics/RestingHeartRateMetric.swift
@@ -0,0 +1,49 @@
+//
+//  RestingHeartRateMetric.swift
+//  FatigueManagementSystem
+//
+//  Created by Sukhman Kaur Kang  on 16/9/2025.
+//
+
+import Foundation
+import HealthKit
+
+class RestingHeartRateMetric: FatigueMetric {
+    func normalisedValue() -> Double {
+        <#code#>
+    }
+    
+    func calculateBaseline() {
+        <#code#>
+    }
+    
+    
+    
+    let name = "restingheartrate"
+    let weight: Double
+    var baseline: Double
+    var rawValue: Double
+    
+    let healthStore: HKHealthStore
+    
+    init(weight: Double, healthStore: HKHealthStore) {
+        self.weight = weight;
+        self.baseline = 60.0
+        self.rawValue = 72.0
+        self.healthStore - healthStore
+        
+        getRawValue()
+        
+        
+    }
+    
+    func getRawValue() {
+        getLatestRestingHearRate { bpm in self.rawValue = bpm
+        }
+    }
+    
+    private func getLatestRestingHeartRate(completion: @escaping (Double) -> Void) {
+        guard let
+    }
+}
+
