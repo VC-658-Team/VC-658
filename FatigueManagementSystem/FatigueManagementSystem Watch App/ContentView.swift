e8adca24f8c838e9ca20974616e2d86d0ee1cd3b
s3989834 <s3989834@student.rmit.edu.au>
2025-09-17 14:11:43 +1000

    Update ContentView.swift

--- FatigueManagementSystem/FatigueManagementSystem Watch App/ContentView.swift
+++ FatigueManagementSystem/FatigueManagementSystem Watch App/ContentView.swift
@@ -6,16 +6,41 @@
 //
 
 import SwiftUI
+import HealthKit
 
 struct ContentView: View {
+    @State private var fatigueScore: Int = 0
+    @State private var restingHR: Double = 65.0
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Resting HR: \(Int(restingHR)) bpm")
+                .font(.headline)
+            
+            Text("Fatigue Score: \(fatigueScore)")
+                .font(.largeTitle)
+                .padding()
+            //Image(systemName: "globe")
+              //  .imageScale(.large)
+                //.foregroundStyle(.tint)
+            //Text("Hello, world!")
         }
-        .padding()
+        .onAppear {
+            testRestingHRMetric()
+        }
+        //.padding()
+    }
+    
+    func testRestingHRMetric() {
+        let healthStore = HKHealthStore()
+        
+        let rhrMetric = RestingHeartRateMetric(weight: 1.0, healthStore: healthStore, rawValue: restingHR)
+        
+        let normalised =rhrMetric.normalisedValue()
+        
+        fatigueScore = Int(normalised * 100)
+        
     }
 }
 
