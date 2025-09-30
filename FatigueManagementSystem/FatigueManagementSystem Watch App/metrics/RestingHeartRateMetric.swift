dd8c6dd029bb5128e6675ef92c9222485c891368
s3989834 <s3989834@student.rmit.edu.au>
2025-09-30 11:59:58 +1000

    Removing some unused coding

--- FatigueManagementSystem/FatigueManagementSystem Watch App/metrics/RestingHeartRateMetric.swift
+++ FatigueManagementSystem/FatigueManagementSystem Watch App/metrics/RestingHeartRateMetric.swift
@@ -7,64 +7,6 @@
 import Foundation
 import HealthKit
 
-/*class RestingHeartRateMetric{
-    private let healthStore = HKHealthStore()
-    
-    // GET AUTHORIZATION
-    func requestAuthorization(completion: @escaping (Bool) -> Void){
-        guard let restingHR = HKQuantityType.quantityType(forIdentifier: .restingHeartRate) else{
-            completion(false)
-            return
-        }
-        
-        let readTypes: Set<HKObjectType> = [restingHR]
-        
-        healthStore.requestAuthorization(toShare: nil, read: readTypes) { success, error in
-            if  let error = error {
-                print ("Healthkiit authorization error : \(error.localizedDescription)")
-            }
-            completion(success)
-            
-        }
-    }
-    
-    //FETCH LATEST RESTING HEART RATE
-    
-    func fetchLatest(completion: @escaping (Double?) -> Void) {
-        guard let restingHRType = HKQuantityType.quantityType(forIdentifier: .restingHeartRate) else {
-            completion(nil)
-            return
-        }
-        
-        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
-        let query = HKSampleQuery(sampleType: restingHRType, predicate: nil, limit: 1, sortDescriptors: [sortDescriptor]
-        ) { _,results, _ in
-            guard let sample = results?.first as? HKQuantitySample else {
-                completion(nil)
-                return
-            }
-            
-            let bpm = sample.quantity.doubleValue(for: HKUnit.count().unitDivided(by: .minute()))
-            completion(bpm)
-        }
-        
-        healthStore.execute(query)
-    }
-    
-    
-    //NORMALISE
-    
-    func normalize(_value: Double) -> Double {
-        let minHR = 40.0
-        let maxHR = 100.0
-        let clamped = max(min(_value, maxHR), minHR)
-        return (clamped - minHR) / (maxHR - minHR)
-    }
-}
- */
-// above is the code which is so far
-//----------------------------------------------------------------------
-//below is testing yet
 
 class RestingHeartRateMetric: FatigueMetric {
     
@@ -120,26 +62,10 @@ class RestingHeartRateMetric: FatigueMetric {
         return (clamped - minHR) / (maxHR - minHR)
     }
 }
-        //getRawValue()
-        
-        //}
-        
-        //func getRawValue() {
-        //bpm in self.rawValue = bpm
-        //  }
-        //func calculateBaseline() {
-        //  baseline = 60.0
-        
-        //}
-        
-        //func normalisedValue() -> Double {
-        
-        //  let ratio = baseline / rawValue
-        //return max(0, min(1, ratio))
-        //}
         
         
-        //}
+        
+      
         
         
         
